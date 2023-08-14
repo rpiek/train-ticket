01188b46fa342cc40314452791066c1b9178ab63
rowan piek <rowan.piek@teampicnic.com>
2023-08-14 16:44:34 +0200

    Removed all extended services

--- ts-preserve-service/src/main/java/preserve/service/PreserveServiceImpl.java
+++ ts-preserve-service/src/main/java/preserve/service/PreserveServiceImpl.java
@@ -49,22 +49,22 @@ public class PreserveServiceImpl implements PreserveService {
         //1.detect ticket scalper
         //PreserveServiceImpl.LOGGER.info("[Step 1] Check Security");
 
-        Response result = checkSecurity(oti.getAccountId(), headers);
-        if (result.getStatus() == 0) {
-            PreserveServiceImpl.LOGGER.error("[preserve][Step 1][Check Security Fail][AccountId: {}]",oti.getAccountId());
-            return new Response<>(0, result.getMsg(), null);
-        }
-        PreserveServiceImpl.LOGGER.info("[preserve][Step 1][Check Security Complete][AccountId: {}]",oti.getAccountId());
+//        Response result = checkSecurity(oti.getAccountId(), headers);
+//        if (result.getStatus() == 0) {
+//            PreserveServiceImpl.LOGGER.error("[preserve][Step 1][Check Security Fail][AccountId: {}]",oti.getAccountId());
+//            return new Response<>(0, result.getMsg(), null);
+//        }
+//        PreserveServiceImpl.LOGGER.info("[preserve][Step 1][Check Security Complete][AccountId: {}]",oti.getAccountId());
         //2.Querying contact information -- modification, mediated by the underlying information micro service
         //PreserveServiceImpl.LOGGER.info("[Step 2] Find contacts");
         //PreserveServiceImpl.LOGGER.info("[Step 2] Contacts Id: {}", oti.getContactsId());
 
-        Response<Contacts> gcr = getContactsById(oti.getContactsId(), headers);
-        if (gcr.getStatus() == 0) {
-            PreserveServiceImpl.LOGGER.error("[preserve][Step 2][Find Contacts Fail][ContactsId: {},message: {}]",oti.getContactsId(),gcr.getMsg());
-            return new Response<>(0, gcr.getMsg(), null);
-        }
-        PreserveServiceImpl.LOGGER.info("[preserve][Step 2][Find contacts Complete][ContactsId: {}]",oti.getContactsId());
+//        Response<Contacts> gcr = getContactsById(oti.getContactsId(), headers);
+//        if (gcr.getStatus() == 0) {
+//            PreserveServiceImpl.LOGGER.error("[preserve][Step 2][Find Contacts Fail][ContactsId: {},message: {}]",oti.getContactsId(),gcr.getMsg());
+//            return new Response<>(0, gcr.getMsg(), null);
+//        }
+//        PreserveServiceImpl.LOGGER.info("[preserve][Step 2][Find contacts Complete][ContactsId: {}]",oti.getContactsId());
         //3.Check the info of train and the number of remaining tickets
         //PreserveServiceImpl.LOGGER.info("[Step 3] Check tickets num");
         TripAllDetailInfo gtdi = new TripAllDetailInfo();
@@ -100,7 +100,7 @@ public class PreserveServiceImpl implements PreserveService {
         PreserveServiceImpl.LOGGER.info("[preserve][Step 3][Check tickets num][Tickets Enough]");
         //4.send the order request and set the order information
         //PreserveServiceImpl.LOGGER.info("[Step 4] Do Order");
-        Contacts contacts = gcr.getData();
+//        Contacts contacts = gcr.getData();
         Order order = new Order();
         UUID orderId = UUID.randomUUID();
         order.setId(orderId.toString());
@@ -114,9 +114,9 @@ public class PreserveServiceImpl implements PreserveService {
         order.setTo(toStationName);
         order.setBoughtDate(StringUtils.Date2String(new Date()));
         order.setStatus(OrderStatus.NOTPAID.getCode());
-        order.setContactsDocumentNumber(contacts.getDocumentNumber());
-        order.setContactsName(contacts.getName());
-        order.setDocumentType(contacts.getDocumentType());
+        order.setContactsDocumentNumber("123");
+        order.setContactsName("John Doe");
+        order.setDocumentType(0);
 
         Travel query = new Travel();
         query.setTrip(trip);
@@ -215,47 +215,47 @@ public class PreserveServiceImpl implements PreserveService {
         }
 
         //7.add consign
-        if (null != oti.getConsigneeName() && !"".equals(oti.getConsigneeName())) {
-
-            Consign consignRequest = new Consign();
-            consignRequest.setOrderId(cor.getData().getId());
-            consignRequest.setAccountId(cor.getData().getAccountId());
-            consignRequest.setHandleDate(oti.getHandleDate());
-            consignRequest.setTargetDate(cor.getData().getTravelDate().toString());
-            consignRequest.setFrom(cor.getData().getFrom());
-            consignRequest.setTo(cor.getData().getTo());
-            consignRequest.setConsignee(oti.getConsigneeName());
-            consignRequest.setPhone(oti.getConsigneePhone());
-            consignRequest.setWeight(oti.getConsigneeWeight());
-            consignRequest.setWithin(oti.isWithin());
-            LOGGER.info("CONSIGN INFO : " +consignRequest.toString());
-            Response icresult = createConsign(consignRequest, headers);
-            if (icresult.getStatus() == 1) {
-                PreserveServiceImpl.LOGGER.info("[preserve][Step 7][Add Consign][Consign Success]");
-            } else {
-                PreserveServiceImpl.LOGGER.error("[preserve][Step 7][Add Consign][Preserve Consign Fail][OrderId: {}]", cor.getData().getId());
-                returnResponse.setMsg("Consign Fail.");
-            }
-        } else {
-            PreserveServiceImpl.LOGGER.info("[preserve][Step 7][Add Consign][Do not need to consign]");
-        }
+//        if (null != oti.getConsigneeName() && !"".equals(oti.getConsigneeName())) {
+//
+//            Consign consignRequest = new Consign();
+//            consignRequest.setOrderId(cor.getData().getId());
+//            consignRequest.setAccountId(cor.getData().getAccountId());
+//            consignRequest.setHandleDate(oti.getHandleDate());
+//            consignRequest.setTargetDate(cor.getData().getTravelDate().toString());
+//            consignRequest.setFrom(cor.getData().getFrom());
+//            consignRequest.setTo(cor.getData().getTo());
+//            consignRequest.setConsignee(oti.getConsigneeName());
+//            consignRequest.setPhone(oti.getConsigneePhone());
+//            consignRequest.setWeight(oti.getConsigneeWeight());
+//            consignRequest.setWithin(oti.isWithin());
+//            LOGGER.info("CONSIGN INFO : " +consignRequest.toString());
+//            Response icresult = createConsign(consignRequest, headers);
+//            if (icresult.getStatus() == 1) {
+//                PreserveServiceImpl.LOGGER.info("[preserve][Step 7][Add Consign][Consign Success]");
+//            } else {
+//                PreserveServiceImpl.LOGGER.error("[preserve][Step 7][Add Consign][Preserve Consign Fail][OrderId: {}]", cor.getData().getId());
+//                returnResponse.setMsg("Consign Fail.");
+//            }
+//        } else {
+//            PreserveServiceImpl.LOGGER.info("[preserve][Step 7][Add Consign][Do not need to consign]");
+//        }
 
         //8.send notification
 
-        User getUser = getAccount(order.getAccountId().toString(), headers);
+//        User getUser = getAccount(order.getAccountId().toString(), headers);
 
-        NotifyInfo notifyInfo = new NotifyInfo();
-        notifyInfo.setDate(new Date().toString());
-
-        notifyInfo.setEmail(getUser.getEmail());
-        notifyInfo.setStartPlace(order.getFrom());
-        notifyInfo.setEndPlace(order.getTo());
-        notifyInfo.setUsername(getUser.getUserName());
-        notifyInfo.setSeatNumber(order.getSeatNumber());
-        notifyInfo.setOrderNumber(order.getId().toString());
-        notifyInfo.setPrice(order.getPrice());
-        notifyInfo.setSeatClass(SeatClass.getNameByCode(order.getSeatClass()));
-        notifyInfo.setStartTime(order.getTravelTime().toString());
+//        NotifyInfo notifyInfo = new NotifyInfo();
+//        notifyInfo.setDate(new Date().toString());
+//
+//        notifyInfo.setEmail(getUser.getEmail());
+//        notifyInfo.setStartPlace(order.getFrom());
+//        notifyInfo.setEndPlace(order.getTo());
+//        notifyInfo.setUsername(getUser.getUserName());
+//        notifyInfo.setSeatNumber(order.getSeatNumber());
+//        notifyInfo.setOrderNumber(order.getId().toString());
+//        notifyInfo.setPrice(order.getPrice());
+//        notifyInfo.setSeatClass(SeatClass.getNameByCode(order.getSeatClass()));
+//        notifyInfo.setStartTime(order.getTravelTime().toString());
 
         // TODO: change to async message serivce
         // sendEmail(notifyInfo, headers);
