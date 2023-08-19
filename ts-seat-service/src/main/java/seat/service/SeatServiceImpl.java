4b3b9f5baf78aaf5ec0b336cc56e12f5c17d9419
rowan piek <rowan.piek@teampicnic.com>
2023-08-19 14:26:29 +0200

    Removed unwanted code edits

--- ts-seat-service/src/main/java/seat/service/SeatServiceImpl.java
+++ ts-seat-service/src/main/java/seat/service/SeatServiceImpl.java
@@ -5,6 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -25,6 +27,9 @@ import java.util.Set;
 public class SeatServiceImpl implements SeatService {
     @Autowired
     RestTemplate restTemplate;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SeatServiceImpl.class);
 
