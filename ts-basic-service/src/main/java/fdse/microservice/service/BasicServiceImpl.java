fad2310d0f16f73f305193b4905084d73540a06b
rowan piek <rowan.piek@teampicnic.com>
2023-08-19 17:03:03 +0200

    Removed unwanted code changes

--- ts-basic-service/src/main/java/fdse/microservice/service/BasicServiceImpl.java
+++ ts-basic-service/src/main/java/fdse/microservice/service/BasicServiceImpl.java
@@ -8,7 +8,7 @@ import edu.fudan.common.util.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -27,8 +27,8 @@ public class BasicServiceImpl implements BasicService {
     @Autowired
     private RestTemplate restTemplate;
 
-//    @Autowired
-//    private DiscoveryClient discoveryClient;
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BasicServiceImpl.class);
 
