457a25d3596449f8150861327a2e6e579553a617
rowan piek <rowan.piek@teampicnic.com>
2023-08-31 15:00:38 +0200

    Added logging

--- ts-security-service/src/main/java/security/config/DataSourceConfig.java
+++ ts-security-service/src/main/java/security/config/DataSourceConfig.java
@@ -1,6 +1,7 @@
 package security.config;
 
 import javax.sql.DataSource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@Slf4j
 @EnableTransactionManagement
 public class DataSourceConfig {
 
@@ -16,7 +18,9 @@ public class DataSourceConfig {
     @Bean(name = "dataSource1")
     @ConfigurationProperties(prefix = "spring.datasource.datasource1")
     public DataSource dataSource1() {
-        return DataSourceBuilder.create().build();
+        DataSource dataSource = DataSourceBuilder.create().build();
+        DataSourceConfig.log.info("dataSource1 URL: {}", dataSource.toString());
+        return dataSource;
     }
 
     @Bean(name = "dataSource2")
