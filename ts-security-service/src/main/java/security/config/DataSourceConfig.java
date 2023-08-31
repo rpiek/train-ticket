03913572cca1a603068d31ba0c36e5c3abf249df
rowan piek <rowan.piek@teampicnic.com>
2023-08-31 15:11:14 +0200

    Removed placeholders in jdbc

--- ts-security-service/src/main/java/security/config/DataSourceConfig.java
+++ ts-security-service/src/main/java/security/config/DataSourceConfig.java
@@ -1,7 +1,6 @@
 package security.config;
 
 import javax.sql.DataSource;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -10,7 +9,6 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@Slf4j
 @EnableTransactionManagement
 public class DataSourceConfig {
 
@@ -18,9 +16,7 @@ public class DataSourceConfig {
     @Bean(name = "dataSource1")
     @ConfigurationProperties(prefix = "spring.datasource.datasource1")
     public DataSource dataSource1() {
-        DataSource dataSource = DataSourceBuilder.create().build();
-        DataSourceConfig.log.info("dataSource1 URL: {}", dataSource.toString());
-        return dataSource;
+        return DataSourceBuilder.create().build();
     }
 
     @Bean(name = "dataSource2")
