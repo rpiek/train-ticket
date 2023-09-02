b1669fe1373fe7be90d50d3da9e2355b7aca701b
rowan piek <rowan.piek@teampicnic.com>
2023-09-02 15:51:43 +0200

    Also added json desrialization

--- ts-basic-service/src/main/java/fdse/microservice/config/WebConfig.java
+++ ts-basic-service/src/main/java/fdse/microservice/config/WebConfig.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -14,8 +15,13 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplateBuilder()
-                .messageConverters(protobufHttpMessageConverter())
+                .messageConverters(protobufHttpMessageConverter(), jsonMessageConverter())
                 .build();
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter jsonMessageConverter() {
+        return new MappingJackson2HttpMessageConverter();
     }
 
     @Bean
