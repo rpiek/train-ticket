08a6f4cdb54eed9fbc998b783190a9dd2e354ca1
rowan piek <rowan.piek@teampicnic.com>
2023-09-18 13:15:09 +0200

    Inherited develop

--- ts-price-service/src/main/java/price/config/SecurityConfig.java
+++ ts-price-service/src/main/java/price/config/SecurityConfig.java
@@ -72,15 +72,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/api/v1/priceservice/**").permitAll()
-                .antMatchers(HttpMethod.POST, prices).hasAnyRole(admin)
-                .antMatchers(HttpMethod.DELETE, prices).hasAnyRole(admin)
-                .antMatchers(HttpMethod.PUT, prices).hasAnyRole(admin)
-                .antMatchers("/swagger-ui.html", "/webjars/**", "/images/**",
-                        "/configuration/**", "/swagger-resources/**", "/v2/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .addFilterBefore(new JWTFilter(), UsernamePasswordAuthenticationFilter.class);
+//                .antMatchers("/api/v1/priceservice/**").permitAll()
+//                .antMatchers(HttpMethod.POST, prices).hasAnyRole(admin)
+//                .antMatchers(HttpMethod.DELETE, prices).hasAnyRole(admin)
+//                .antMatchers(HttpMethod.PUT, prices).hasAnyRole(admin)
+//                .antMatchers("/swagger-ui.html", "/webjars/**", "/images/**",
+//                        "/configuration/**", "/swagger-resources/**", "/v2/**").permitAll()
+                .anyRequest().permitAll();
+//                .and()
+//                .addFilterBefore(new JWTFilter(), UsernamePasswordAuthenticationFilter.class);
 
         // close cache
         httpSecurity.headers().cacheControl();
